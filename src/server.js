b00b3dc73fc5b7d320d7c8ddb4844dbf015dfb24
bradleyc95 <bradleycox1995@gmail.com>
2024-02-19 12:49:49 -0500

    setup of server and dependencies

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
+});

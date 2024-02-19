faac88763508c669932deb07a5f9b20d1ec42b7e
bradleyc95 <bradleycox1995@gmail.com>
2024-02-19 15:46:06 -0500

    Finished user registration, login, logout, added ticket submission and ticket display (admin access)

--- src/foundationsFunctions.js
+++ src/foundationsFunctions.js
@@ -1,6 +1,10 @@
 const {logger} = require('./util/logger');
 
 const users = [];
+const tickets = [];
+const pendingTickets = [];
+const currentUser = [];
+let ticketID = 0;
 
 function createNewUser(username, password, admin = false) {
     const newUser = {
@@ -13,7 +17,70 @@ function createNewUser(username, password, admin = false) {
     return `${username} has been registered`;
 }
 
+function logout() {
+    let user = currentUser.splice(0, 1);
+    logger.info(`Logout successful: ${user[0].username}`);
+    return `User ${user[0].username} successfully logged out`;
+}
+
+function login(username, password) {
+    for (let i = 0; i < users.length; i++) {
+        // CHECK FOR VALID CREDENTIALS
+        if (users[i].username == username && users[i].password == password) {
+            // RESET CURRENT USER
+            currentUser.splice(0, 1);
+            currentUser.push(users[i]);
+            const message = currentUser[0].admin ? `Admin ${currentUser[0].username} logged in successfully` : `Employee ${currentUser[0].username} logged in successfully`;
+            logger.info(`Login successful: ${username}`);
+            return message;
+        }
+    }
+    // IF UNABLE TO VALIDATE CREDENTIALS, RETURN 0
+    logger.error(`Invalid username or password: ${username}`);
+    return 0;
+}
+
+function submitTicket(description, type, amount) {
+    const newTicket = {
+        id: ticketID,
+        author: currentUser[0].username,
+        status: 'pending',
+        description,
+        type,
+        amount
+    }
+    tickets.push(newTicket);
+    logger.info(`Ticket created successfully (ID: ${ticketID}, User: ${currentUser[0].username})`);
+    ticketID++;
+    return `Ticket #${ticketID} submitted by user ${currentUser[0].username} successfully`;
+}
+
+function displayPendingTickets() {
+    if (tickets.length == 0) {
+        return 'There are currently no pending tickets to display';
+    } else {
+        // RESET CURRENT LIST OF PENDINGTICKETS BEFORE DISPLAY
+        pendingTickets.splice(0, pendingTickets.length);
+
+        // ADD ALL TICKETS WITH STATUS PENDING TO PENDINGTICKETS
+        for (let i = 0; i < tickets.length; i++) {
+            if (tickets[i].status == 'pending') {
+                pendingTickets.push(tickets[i]);
+            }
+        }
+        logger.info(`Displaying ${pendingTickets.length} pending tickets`);
+        return `There are currently ${pendingTickets.length} pending tickets`;
+    }
+}
+
 module.exports = {
     users,
-    createNewUser
+    currentUser,
+    tickets,
+    pendingTickets,
+    createNewUser,
+    logout,
+    login,
+    submitTicket,
+    displayPendingTickets
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const PORT = 3000;
 
-const {users, createNewUser} = require('./foundationsFunctions');
+const {users, currentUser, tickets, pendingTickets, createNewUser, logout, login, submitTicket, displayPendingTickets} = require('./foundationsFunctions');
 
 const server = http.createServer((req, res) => {
     let body = '';
-
+    
     req.on('data', (chunk) => {
         body += chunk;
     })
@@ -13,6 +13,7 @@ const server = http.createServer((req, res) => {
         body = body.length > 0 ? JSON.parse(body) : {};
         const contentType = {'Content-Type': 'application/json'};
 
+        // USER LOGIN AND REGISTRATION
         if (req.method == 'POST' && req.url == '/register') {
             // REGISTERING NEW USERS
             const {username, password, admin} = body;
@@ -36,8 +37,71 @@ const server = http.createServer((req, res) => {
                 res.end(JSON.stringify({postMessage, users}));
             }
 
+        // USER LOGIN
         } else if (req.method == 'POST' && req.url == '/login') {
+            const {username, password} = body;
 
+            // ERROR HANDLING: MISSING FIELDS
+            if (!username || !password) {
+                res.writeHead(400, contentType);
+                res.end(JSON.stringify({message: 'Please provide a username and password'}));
+            } else {
+                const postMessage = login(username, password);
+
+                // login returns 0 on fail
+                if (postMessage == 0) {
+                    res.writeHead(400, contentType);
+                    res.end(JSON.stringify({message: 'Username or password do not match any profiles on record'}));
+                } else {
+                    res.writeHead(201, contentType);
+                    let user = currentUser[0];
+                    res.end(JSON.stringify({postMessage, user}));
+                }
+            }
+
+        // LOGOUT CURRENT USER -- MOSTLY FOR TESTING PURPOSES
+        } else if (req.method == 'POST' && req.url == '/logout') {
+            if (currentUser.length > 0) {
+                const postMessage = logout();
+                res.writeHead(201, contentType);
+                res.end(JSON.stringify({postMessage}));
+            } else {
+                res.writeHead(400, contentType);
+                res.end(JSON.stringify({message: 'No user is currently logged in'}));
+            }
+            
+        // TICKET SUBMISSION
+        } else if (req.method == 'POST' && req.url == '/tickets') {
+            const {description, type, amount} = body;
+
+            // ERROR HANDLING
+            if (currentUser.length == 0) {
+                // NOT LOGGED IN
+                res.writeHead(400, contentType);
+                res.end(JSON.stringify({message: 'You must be logged in to submit a ticket'}))
+            } else if (!description || !type || !amount) {
+                // MISSING FIELD
+                res.writeHead(400, contentType);
+                res.end(JSON.stringify({message: 'Unable to submit ticket, tickets MUST contain a description, type, and amount'}));
+            } else {
+                // CREATE NEW TICKET
+                const postMessage = submitTicket(description, type, amount);
+                res.writeHead(201, contentType);
+                res.end(JSON.stringify({postMessage}));
+            }
+        
+        // TICKETING SYSTEM -- VIEW PENDING TICKETS (ADMIN ACCESS ONLY)
+        } else if (req.method == 'GET' && req.url == '/tickets?status=Pending') {
+            // CHECK FOR ADMIN PRIVELEGES
+            if (currentUser[0].admin == false) {
+                res.writeHead(400, contentType);
+                res.end(JSON.stringify({message: 'Access denied -- You do not have administrative privileges'}));
+            } else {
+                // ACCESS GRANTED -- DISPLAY ALL PENDING TICKETS
+                const postMessage = displayPendingTickets();
+                res.writeHead(201, contentType);
+                res.end(JSON.stringify({postMessage, pendingTickets}));
+            }
         }
 
     });
